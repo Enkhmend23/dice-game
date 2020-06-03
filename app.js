bc2f1abc476b396afe145fa9e711ee3dc476c1fd
mendee <bemmendee@gmail.com>
2020-06-03 20:43:31 +0800

    Toglogchiin eeljiig solidog

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
@@ -24,10 +24,42 @@ document.getElementById("score-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 
+// Шоог шидэх эвент листенер
 // Anonymous буюу нэргүй функц ашиглав
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  // Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө
   var diceNumber = Math.floor(Math.random() * 6) + 1;
 
+  // Шооны зургийг вэб дээр гаргаж ирнэ.
   diceDom.style.display = "block";
+
+  // Буусан санамсаргүй тоонд харгалзах шооны зургийг вэб дээр гаргаж ирнэ.
   diceDom.src = "dice-" + diceNumber + ".png";
+
+  // Буусан тоо нь 1-ээс ялгаатай бол идэвхтэй тоглогчийн ээлжийн оноог нэмэгдүүлнэ.
+  if (diceNumber !== 1) {
+    // 1-ээс ялгаатай тоо буулаа
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    // 1 буусан тул тоглогчийн ээлжийг энэ хэсэгт сольж өгнө
+
+    // Энэ тоглогчийн ээлжийг энэ хэсэгт сольж өгнө
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+
+    //Тоглогчийн ээлжийг нөгөө тоглогч руу шилжүүлнэ
+    // Хэрэв идэвхтэй тоглогч нь 0 байвал идвэхтэй тоглогчийг 1 болго
+    // Үгүй бол идвэхтэй тоглогчийг 0 болго
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0); //гурвалсан оператор
+
+    // Улаан цэгийг шилжүүлэх код
+    //document.querySelector(".player-0-panel").classList.remove("active");
+    //document.querySelector(".player-1-panel").classList.add("active");
+    document.querySelector(".player-0-panel").classList.toggle("active"); // toggle одоо байгаа төлөвийг нь нэмэх, эсвэл устгах эсэргээр нь нөлөөлнө
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    // шоог үр алга болгох
+    diceDom.style.display = "none";
+  }
 });
